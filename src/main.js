bb0d341d0ea0430879335d1daed81349a9fe898a
spica1108 <815303067@qq.com>
2025-09-03 18:05:16 +0800

    layout吸顶导航交互实现

--- src/main.js
+++ src/main.js
@@ -8,11 +8,6 @@ import App from './App.vue'
 import router from './router'
 
 //测试接口函数，导入
-import { testAPI } from '@/apis/testAPI'
-//调用函数，尝试获取结果
-testAPI().then(res =>  {
-  console.log(res)
-})
 const app = createApp(App)
 
 app.use(createPinia())
