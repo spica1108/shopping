b004d6dbda8d162b1c100b1d62a5e8d02ae9cfcf
spica1108 <815303067@qq.com>
2025-09-03 23:12:53 +0800

    面板组件封装

---
+++ src/views/Home/components/HomePanel.vue
@@ -0,0 +1,61 @@
+<script setup>
+
+defineProps({
+  title: {
+    type: String,
+    default: ''
+  },
+  subTitle: {
+    type: String,
+    default: ''
+  }
+})
+
+</script>
+
+
+<template>
+  <div class="home-panel">
+    <div class="container">
+      <div class="head">
+        <!-- 主标题和副标题 -->
+        <h3>
+          {{ title }}
+            <small>{{ subTitle }}</small>
+        </h3>
+      </div>
+      <!-- 主体内容区域 -->
+      <!-- 插槽，占位符，在组件模板中预留一个空位
+      使用组件时可以往这个空位里塞入自定义的HTML内容 -->
+      <slot name="main" />
+    </div>
+  </div>
+</template>
+
+
+<style scoped lang='scss'>
+.home-panel {
+  background-color: #fff;
+
+  .head {
+    padding: 40px 0;
+    display: flex;
+    align-items: flex-end;
+
+    h3 {
+      flex: 1;
+      font-size: 32px;
+      font-weight: normal;
+      margin-left: 6px;
+      height: 35px;
+      line-height: 35px;
+
+      small {
+        font-size: 16px;
+        color: #999;
+        margin-left: 20px;
+      }
+    }
+  }
+}
+</style>
