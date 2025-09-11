aaf5deb347c74e84d242a1481d2e50222fea168b
spica1108 <815303067@qq.com>
2025-09-11 11:35:29 +0800

    结算-路由配置和基础数据渲染

--- src/views/CartList/index.vue
+++ src/views/CartList/index.vue
@@ -91,7 +91,8 @@ const allCheck = (selected) => {
           <span class="red">¥ {{ cartStore.selectedPrice.toFixed(2) }} </span>
         </div>
         <div class="total">
-          <el-button size="large" type="primary" >下单结算</el-button>
+          <!-- 路由跳转 -->
+          <el-button size="large" type="primary" @click="$router.push('/checkout')">下单结算</el-button>
         </div>
       </div>
     </div>
